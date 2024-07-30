3b4abe90c7e38ec91bef57071b3aedafa4a78d81
Aslıgül Kaya <asligul.kaya01@gmail.com>
2024-07-30 16:08:54 +0300

    403 Not Authorized Page UI Design

--- src/views/NotAuthorized/NotAuthorized.jsx
+++ src/views/NotAuthorized/NotAuthorized.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import classes from "./NotAuthorized.module.css";
+import notAuthorized from "../../assets/notAuthorized.png";
 
 const NotAuthorized = () => (
-  <div>
-    <h1>403 - Not Authorized</h1>
-    <p>You do not have permission to view this page.</p>
-    <Link to="/">Go to Homepage</Link>
+  <div className="d-flex align-items-center" style={{ height: "90vh" }}>
+    <section className="w-50">
+      <h1 className={classes.header403}>403</h1>
+      <h2>Not Authorized</h2>
+      <p>You do not have permission to view this page.</p>
+      <Link to="/">Go to Homepage</Link>
+    </section>
+    <section className="w-50">
+      <img src={notAuthorized} alt="not found" style={{ width: "600px" }} />
+    </section>
   </div>
 );
 
