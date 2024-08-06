b6dea7161d5b2314c0ca3899f676f579c0a5318d
Aslıgül Kaya <asligul.kaya01@gmail.com>
2024-08-06 12:58:27 +0300

    Percentage of battery

--- src/components/Battery/Battery.jsx
+++ src/components/Battery/Battery.jsx
@@ -10,8 +10,8 @@ const Battery = ({ level }) => {
 
   const rectangles = Array.from({ length: 4 }, (_, i) => {
     const fillLevel = Math.max(0, Math.min(level - i * 25, 25));
-    const height = (fillLevel / 25) * 80; // Height decreases with battery level
-    const y = 365 - i * 105 + (80 - height); // Adjust y position based on height
+    const height = (fillLevel / 25) * 80;
+    const y = 365 - i * 105 + (80 - height);
 
     return (
       <rect
@@ -42,6 +42,15 @@ const Battery = ({ level }) => {
           d="M319.089,27.221h-36.475V0h-95.27v27.221h-34.607c-22.517,0-40.829,18.317-40.829,40.832v362.946 c0,22.51,18.317,40.83,40.829,40.83h166.352c22.524,0,40.832-18.32,40.832-40.83V68.052 C359.921,45.538,341.613,27.221,319.089,27.221z M332.705,431.002c0,7.501-6.108,13.607-13.616,13.607H152.737 c-7.501,0-13.608-6.095-13.608-13.607V68.052c0-7.501,6.107-13.611,13.608-13.611h166.352c7.508,0,13.616,6.109,13.616,13.611"
         />
         {rectangles}
+        <text
+          x="235"
+          y="100"
+          textAnchor="middle"
+          fontSize="40"
+          fill="white"
+        >
+          {level}%
+        </text>
       </svg>
     </div>
   );
