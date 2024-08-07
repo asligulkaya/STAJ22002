dc0f7206f6403d4e3adef363d76928e8a925bee2
Aslıgül Kaya <asligul.kaya01@gmail.com>
2024-08-07 11:54:20 +0300

    electron integration

---
+++ main.js
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false
+    }
+  });
+
+  mainWindow.loadURL('http://localhost:5173');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
