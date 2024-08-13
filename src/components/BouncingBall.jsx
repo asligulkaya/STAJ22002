ab516758c0f2ae019f2d4f5b0929e353ef3d03a6
Aslıgül Kaya <asligul.kaya01@gmail.com>
2024-08-13 16:56:00 +0300

    sound fix

--- src/components/BouncingBall.jsx
+++ src/components/BouncingBall.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 
 const BouncingBall = ({ color, speed, soundEnabled, isPlaying }) => {
   const mountRef = useRef(null);
   const sound = useRef(new Audio("/sound.mp3"));
+  sound.current.loop = true;
+  const [isUserInteracted, setIsUserInteracted] = useState(false);
 
   useEffect(() => {
     const scene = new THREE.Scene();
@@ -35,10 +37,14 @@ const BouncingBall = ({ color, speed, soundEnabled, isPlaying }) => {
     const ballRadius = 1;
 
     const animate = function () {
-      if (isPlaying) {
+      if (isPlaying && isUserInteracted) {
         if (positionX > 7 - ballRadius || positionX < -7 + ballRadius) {
           speedX = -speedX;
-          if (soundEnabled) sound.current.play();
+          if (soundEnabled && sound.current && !sound.current.paused) {
+            sound.current.play().catch((error) => {
+              console.error("Sound play error:", error);
+            });
+          }
         }
         positionX += speedX;
         ball.position.x = positionX;
@@ -53,23 +59,44 @@ const BouncingBall = ({ color, speed, soundEnabled, isPlaying }) => {
     return () => {
       mountRef.current.removeChild(renderer.domElement);
     };
-  }, [color, speed, soundEnabled, isPlaying]);
+  }, [color, speed, soundEnabled, isPlaying, isUserInteracted]);
+
+  const handleUserInteraction = () => {
+    setIsUserInteracted(true);
+    if (soundEnabled && sound.current.paused && isPlaying) {
+      sound.current.play().catch((error) => {
+        console.error("Sound play error:", error);
+      });
+    }
+  };
 
   useEffect(() => {
     if (!isPlaying) {
       sound.current.pause();
-      sound.current.currentTime = 0;
+    } else if (isUserInteracted && soundEnabled) {
+      sound.current.play().catch((error) => {
+        console.error("Sound play error:", error);
+      });
     }
   }, [isPlaying]);
 
   useEffect(() => {
     if (!soundEnabled) {
       sound.current.pause();
-      sound.current.currentTime = 0;
+    } else if (isUserInteracted && isPlaying) {
+      sound.current.play().catch((error) => {
+        console.error("Sound play error:", error);
+      });
     }
   }, [soundEnabled]);
 
-  return <div ref={mountRef}></div>;
+  return (
+    <div ref={mountRef}>
+      {!isUserInteracted && (
+        <button onClick={handleUserInteraction}>Start</button>
+      )}
+    </div>
+  );
 };
 
 export default BouncingBall;
